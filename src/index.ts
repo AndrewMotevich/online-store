db993f38848ebcf99bd7dae1f710fa7c92df8407
Timothy7310 <penlist127@gmail.com>
2022-12-27 10:08:15 +0300

    feat: add header section

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {log} from "./components/test";
 import "./style.scss";
-import "./assets/img/github-logo.png";
-log();
+import "./assets/img/logo.png";
+import "./assets/img/basket.png";
+import "./assets/img/gold.png";
