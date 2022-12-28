7ae0818a3ff2936d0e8d97f2e708e407916755d7
Timothy7310 <penlist127@gmail.com>
2022-12-28 17:53:54 +0300

    refactor: replace height

--- src/templates/error-page.js
+++ src/templates/error-page.js
@@ -1,10 +1,8 @@
 const errorPageLayout = `
-<div class="main__wrap">
-    <div class="error">
-        <h2 class="error__title visually-hidden">
-            Страница не найдена
-        </h2>
-    </div>
-</div>
+<section class="error">
+    <h2 class="error__title visually-hidden">
+        Страница не найдена
+    </h2>
+</section>
 `;
 export default errorPageLayout;
