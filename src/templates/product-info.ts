80e058e0954966bc378c934541de879b77a23b54
Timothy7310 <penlist127@gmail.com>
2022-12-30 13:01:44 +0300

    feat: add little improvements

--- src/templates/product-info.ts
+++ src/templates/product-info.ts
@@ -1,39 +1,50 @@
-const productInfo = `<div class="goods-details_wrapper">
+const productInfo = `<div class="goods-details_wrapper container">
 <nav class="goods-details_navigation">
   <ul>
-    <li class="navigation_title"><a href="#">Secret Shop</a></li>
     <li class="navigation_path">
-      <a href="#">/Наборы/</a>
-      <a href="#">PrimalBeast/</a>
+      <a href="#">Secret Shop</a>
     </li>
-    <li class="navigation_active"><a href="#">Dark Behemoth</a></li>
+    <li class="navigation_path">
+      <a href="#">Наборы</a>
+    </li>
+    <li class="navigation_path">
+      <a href="#">PrimalBeast</a>
+    </li>
+    <li class="navigation_path">
+      <a href="#">Dark Behemoth</a>
+    </li>
   </ul>
 </nav>
 <section class="goods-details_info-field">
+  <h2 class="visually-hidden">описание товара</h2>
   <div class="info-field_img">
     <img src="https://collectorsshop.ru/img/catalog/items/main_images/2022/11/625.png"
       alt="goods image">
     <div class="arrows">
-      <p class="arrow-left">&#8592</p>
-      <p class="arrow-right">&#8594</p>
+      <button class="arrow-left">&#8592</button>
+      <button class="arrow-right">&#8594</button>
     </div>
   </div>
   <div class="info-field_description">
-    <h2>Dark Behemoth</h2>
-    <div class="label label_price">1999 руб.</div>
-    <div class="label description_modal-button">Купить в один клик</div>
+    <h3>Dark Behemoth</h3>
+    <span class="label label_price">1999 руб.</span>
+    <button class="label description_modal-button">Купить в один клик</button>
     <div class="description_labels">
-      <div class="label label_hero"><img class="label_hero-icon"
-          src="https://collectorsshop.ru/img/catalog/heroes/mini/2022/11/123.jpeg" alt="hero">Primal Beast</div>
+      <div class="label label_hero">
+        <img class="label_hero-icon"
+          src="https://collectorsshop.ru/img/catalog/heroes/mini/2022/11/123.jpeg" alt="">
+          <span class="label_hero-name">Primal Beast</span>
+        </div>
       <p class="label label_type">Набор</p>
-      <p class="label label_rarity"><svg width="21" height="21" viewBox="0 0 21 21" fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <circle cx="10.5" cy="10.5" r="5.5" fill="#8847FF" />
-          <circle cx="10.5" cy="10.5" r="10" stroke="#8847FF" />
-        </svg>Mythical</p>
-      <p class="label label_stock">На складе:&nbsp<span>1</span></p>
+      <div class="label label_rarity mythical-border">
+        <div class="label_rarity-icon mythical-border">
+          <div class="label_rarity-icon-dot mythical-bg"></div>
+        </div>
+        <span class="label_rarity-name mythical">Mythical</span>
+      </div>
+      <p class="label label_stock">На складе:&nbsp<span class="label_stock-count">1</span></p>
     </div>
-    <h3>Описание товара</h3>
+    <h4>Описание товара</h4>
     <div class="line"></div>
     <p class="description_text">Содержит загрузочный экран и все предметы из набора Dark Behemoth для Primal
       Beast.</p>
