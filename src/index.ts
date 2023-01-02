e62d778a607600cef941bd174974498cb1fb8f6c
timothy <penlist127@gmail.com>
2023-01-02 13:55:26 +0300

    feat: start appView class

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ import "./assets/img/error-bg.jpg";
 import "./assets/img/logo.png";
 import "./assets/img/basket.png";
 import "./assets/img/gold.png";
+import { AppView } from "./components/appView";
+const app = new AppView('Secret Shop', '/', document.querySelector('.main') as HTMLElement);
+app.init();
