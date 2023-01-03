04351a943e9da5b3d37edab3b29ed588fc460c3d
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-01-03 11:02:18 +0300

    feat: add controller class

---
+++ src/components/types.ts
@@ -0,0 +1,16 @@
+type ICard = {
+  'id': number,
+  'item-name': string,
+  'category': string,
+  'type': string,
+  'hero': string,
+  'rarity': string,
+  'description': string,
+  'price': number,
+  'stock': number,
+  'hero-icon': string,
+  'img1': string,
+  'img2': string,
+  'video-link': string
+}
+export {ICard};
