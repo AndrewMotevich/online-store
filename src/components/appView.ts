eb3801915dbb7ebe7e498cd880a3da199ab49980
Timothy7310 <penlist127@gmail.com>
2023-01-08 13:32:02 +0300

    feat: add product class

--- src/components/appView.ts
+++ src/components/appView.ts
@@ -1,6 +1,7 @@
 import MainPage from "./mainPage";
 import router from "./router";
 import { QString } from "./qString";
+import { Product } from "./product";
 
 class Cards {
     mainPage;
@@ -51,8 +52,10 @@ class Cards {
 
 class AppView {
     qString;
+    product;
     constructor() {
         this.qString = new QString();
+        this.product = new Product();
     }
 
     filterChecker() {
@@ -82,10 +85,19 @@ class AppView {
             router.init();
             this.filterChecker();
             this.typeView();
+
+            if (localStorage.getItem('lastPath')?.includes('products/')) {
+                const id = Number(localStorage.getItem('lastPath')?.split('/')[1]);
+                this.product.render(id);
+            }
         });
         
         window.addEventListener('popstate', () => {
             router.init();
+            if (router.getFragment().includes('products/')) {
+                const id = Number(router.getFragment().split('/')[1]);
+                this.product.render(id);
+            }
         });
         
         document.querySelector('.header__logo')?.addEventListener('click', (e) => {
@@ -110,6 +122,8 @@ class AppView {
             if (target.closest('.goods-cards__item')) {
                 e.preventDefault();
                 router.navigate(`products/${target.dataset.id}`, `Secret Shop - Товар`);
+                const id = Number(target.dataset.id);
+                this.product.render(id);
             }
 
             if(target.closest('.goods-filter-input')) {
