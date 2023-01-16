c809e5e31778b591bf2229839ece8252669a71f8
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-01-16 14:34:56 +0300

    feat: add tests to controller

--- src/components/controller.ts
+++ src/components/controller.ts
@@ -1,25 +1,3 @@
-/*//Test controller
-    import { Controller } from "./components/controller";
-    const controller = new Controller();
-    //get all cards
-    const cards = controller.getAllCards();
-    console.log(cards);
-    //get card by Id
-    const cardId = controller.getCardById(3);
-    console.log("Card with ID 3:\n",cardId);
-    //get card by name
-    const cardName = controller.getCardByItemName("Fate Meridian");
-    console.log("Card with item name -Fate Meridian-:\n",cardName);
-    //get cards by category
-    const cardsCategoryInternational2020 = controller.getCardsByCategory("The International 2020");
-    console.log("Cards from category -International 2020-:\n",cardsCategoryInternational2020);
-    //get cards by type
-    const cardsTypeItem = controller.getCardsByType("item");
-    console.log("Cards with type Item:\n",cardsTypeItem);
-    //get cards by rarity
-    const cardsRarityMythical = controller.getCardsByRarity("Mythical");
-    console.log("Cards with rarity Mythical:\n",cardsRarityMythical);
-*/
 import goods from '../assets/goods.json';
 import { ICard } from './types';
 
